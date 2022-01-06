37032b0e6a59846d26c1eca0a3f0a7d531680e30
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-06 20:54:02 +0530

    building createPin component

--- src/components/CreatePin/CreatePin.jsx
+++ src/components/CreatePin/CreatePin.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+import { MdDelete } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
+import { client } from '../../client'
+import { Spinner } from '../Spinner/Spinner'
 
-const CreatePin = () => {
+
+
+const CreatePin = ({ user }) => {
+    const navigate = useNavigate()
+
+
     return (
         <>
             CreatePin
