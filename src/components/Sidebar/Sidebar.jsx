8ce1d75e1e657c975428c02b082be672e919e9b8
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-06 23:41:43 +0530

    added user picture and the banner to the userprofile

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -81,20 +81,20 @@ const Sidebar = ({ user, closeToggle }) => {
                 {user && (
                     <Link
                         to={`user-profile/${user._id}`}
-                        className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+                        className='flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3'
                         onClick={handleCloseSideBar}
                     >
                         <img
                             src={user.image}
-                            className="w-10 h-10 rounded-full"
+                            className='w-10 h-10 rounded-full'
                             alt='user-profile'
                         />
+
 
                         <p>{user.userName}</p>
                     </Link>
                 )}
             </div>
-            <h1>Side Bar</h1>
         </>
     )
 }
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -3,12 +3,59 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { useParams, useNavigate } from 'react-router-dom';
 import { GoogleLogout } from 'react-google-login'
 
-import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../../client'
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../../utils/data'
+import { client } from '../../client'
+import { MasonryLayout } from '../MasonryLayout/MasonryLayout'
+import { Spinner } from '../Spinner/Spinner';
+
+
+const randomImage = 'https://source.unsplash.com/1600x900/?nature,photography,technology?gaming'
+
 
 
 const UserProfile = () => {
+    const [user, setUser] = useState(null);
+    const [pins, setPins] = useState(null);
+    const [text, setText] = useState('Created')
+    const [activeBtn, setActiveBtn] = useState('created')
+
+    const navigate = useNavigate()
+    const { userId } = useParams()
+
+
+    useEffect(() => {
+        const query = userQuery(userId)
+
+        client.fetch(query)
+            .then((data) => {
+                setUser(data[0])
+            })
+    }, [userId])
+
+
+
+    if (!user) return <Spinner message="Loading Profile..." />
     return (
         <>
+            <div className='relative pb-2 h-full justify-center items-center'>
+                <div className='flex flex-col pb-5'>
+                    <div className='relative flex flex-col mb-7'>
+                        <div className='flex flex-col justify-center items-center'>
+                            <img
+                                src={randomImage}
+                                className='w-full h-370 2xl:h-510 shadow-lg object-cover '
+                                alt='banner'
+                            />
+                            <img
+                                className=' rounded-full w-20 h-20 -mt-10 shadow-xl object-cover'
+                                alt='user-pic'
+                                src={user.image}
+                            />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
             <h1>User Profile</h1>
             <h1>User Profiwe</h1>
         </>
