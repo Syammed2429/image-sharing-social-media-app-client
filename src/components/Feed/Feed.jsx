c42f6a18d1b8f256435d2c45ab4d0a7fe0b1286d
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-06 18:29:43 +0530

    fetching the catergory id query on feed

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { client } from '../../client';
+import { searchQuery } from '../../utils/data';
 import { MasonryLayout } from '../MasonryLayout/MasonryLayout'
 import { Spinner } from '../Spinner/Spinner'
 
@@ -10,12 +11,19 @@ import { Spinner } from '../Spinner/Spinner'
 const Feed = () => {
 
     const [loading, setLoading] = useState(false)
+    const [pins, setPins] = useState(null)
     const { categoryId } = useParams()
 
     useEffect(() => {
         if (categoryId) {
             setLoading(true)
-            // const query = searchQuery
+            const query = searchQuery(categoryId)
+
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data)
+                    setLoading(false)
+                })
 
         } else {
 
