aea47d958531d6fc62d7be23ebf4a2c568a66c80
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-06 18:49:32 +0530

    added masonry

--- src/components/MasonryLayout/MasonryLayout.jsx
+++ src/components/MasonryLayout/MasonryLayout.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Masonry from 'react-masonry-css'
+import { Pin } from '../Pin/Pin'
 
-const MasonryLayout = () => {
+const breakpointObj = {
+    default: 4,
+    3000: 6,
+    2000: 5,
+    1200: 3,
+    1000: 2,
+    500: 1
+}
+
+const MasonryLayout = ({ pins }) => {
     return (
         <>
-            MasonryLayout
+            <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj} >
+                {pins?.map((pin) => <Pin key={pin._id} pin={pin} className='w-max' />)}
+            </Masonry>
         </>
     )
 }
---
+++ src/components/Pin/Pin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pin = () => {
+    return (
+        <>
+            Pin
+        </>
+    )
+}
+
+export { Pin }
