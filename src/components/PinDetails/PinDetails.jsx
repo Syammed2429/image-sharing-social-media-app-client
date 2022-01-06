bc3d77cbf81a6ea4f7bcdc0a0226651d208056a1
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-06 22:19:56 +0530

    building pin details

--- src/components/PinDetails/PinDetails.jsx
+++ src/components/PinDetails/PinDetails.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetails = () => {
+import { client, urlFor } from '../../client';
+import MasonryLayout from '../MasonryLayout/MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../../utils/data';
+import { Spinner } from '../Spinner/Spinner';
+
+
+
+
+const PinDetails = ({ user }) => {
+    const [pins, setPins] = useState(null);
+    const [pinDetails, setPinDetails] = useState(null);
+    const [comment, setComment] = useState('');
+    const [addingComment, setAddingComment] = useState(false)
+
+
     return (
         <>
-            Pin Details
 
         </>
     )
