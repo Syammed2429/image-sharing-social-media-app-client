cc741ad655a815847fb508cd850fe7c8d5c37cf0
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-07 10:13:04 +0530

    added alt for logo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -38,7 +38,9 @@ const Login = () => {
 
                 <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
                     <div className="p-5">
-                        <img src={logo} width="130px" />
+                        <img src={logo} width="130px"
+                            alt="logo"
+                        />
                     </div>
 
                     <div className="shadow-2xl">
