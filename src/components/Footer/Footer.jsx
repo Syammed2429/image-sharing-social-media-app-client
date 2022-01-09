653817194b374750d12de7a3161022aef259aca2
Coding With Mr.M <kerry.blig12@gmail.com>
2022-01-09 23:14:07 +0800

    Changed the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,8 @@ const Footer = () => {
         <>
             <div className='relative top-0 left-0 w-100 text-center text-md ' >
 
-                <h2>Made With <span className='text-red-500'>♥️</span> By <a
-                    href="https://github.com/kerrybli"
-                    className="font-bold font-base"
-                >Dada Khalandar</a></h2>
+                <h2>Made With <span className='text-red-500'>♥️</span> By 
+Coding With Mr.M & Adek Ninie</h2>
             </div >
         </>
     )
