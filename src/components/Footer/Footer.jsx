aeb29d56ca064195a7b73b741b4afb6078cb48a8
Dada Khalandar <kerry.blig12@gmail.com>
2024-09-01 07:26:47 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
             <div className='relative top-0 left-0 w-100 text-center text-md ' >
 
                 <h2>Made With <span className='text-red-500'>♥️</span> By 
-Coding With Mr.M & Adek Ninie</h2>
+Coding With Mr.M</h2>
             </div >
         </>
     )
